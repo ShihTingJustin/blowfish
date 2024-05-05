89939fcf6f6f2e3d0965f1524c23383ec63e527a
ShihTingJustin <justinhuang777@gmail.com>
2024-05-05 14:30:06 +0800

    chore: add animation in tailwind, add favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,16 @@ module.exports = {
     "./content/**/*.{html,md}",
     "./themes/blowfish/layouts/**/*.html",
     "./themes/blowfish/content/**/*.{html,md}",
-    "./node_modules/tw-elements/js/**/*.js"
+    "./node_modules/tw-elements/js/**/*.js",
   ],
   darkMode: "class",
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '853px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
+      sm: "640px",
+      md: "853px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     colors: {
       transparent: "transparent",
@@ -117,7 +117,7 @@ module.exports = {
               padding: "0.1rem 0.2rem",
               borderRadius: "0.25rem",
             },
-            code:{
+            code: {
               backgroundColor: theme("colors.neutral.50 / 1"),
               paddingTop: "3px",
               paddingBottom: "3px",
@@ -125,24 +125,24 @@ module.exports = {
               paddingRight: "5px",
               borderRadius: "0.25rem",
             },
-            'code::before': {
-              display: 'none'
+            "code::before": {
+              display: "none",
             },
-            'code::after': {
-              display: 'none'
+            "code::after": {
+              display: "none",
             },
-            'p::before': {
-              display: 'none'
+            "p::before": {
+              display: "none",
             },
-            'p::after': {
-              display: 'none'
+            "p::after": {
+              display: "none",
             },
-            'a.active': {
+            "a.active": {
               "text-decoration-color": theme("colors.primary.600 / 1"),
             },
-            'p.active': {
+            "p.active": {
               "text-decoration-color": theme("colors.primary.600 / 1"),
-            }
+            },
           },
         },
         invert: {
@@ -160,22 +160,32 @@ module.exports = {
             mark: {
               backgroundColor: theme("colors.primary.400 / 1"),
             },
-            code:{
+            code: {
               backgroundColor: theme("colors.neutral.700 / 1"),
             },
-            'a.active': {
-              "text-decoration-color": theme("colors.primary.400 / 1")
+            "a.active": {
+              "text-decoration-color": theme("colors.primary.400 / 1"),
             },
-            'p.active': {
-              "text-decoration-color": theme("colors.primary.400 / 1")
-            }
+            "p.active": {
+              "text-decoration-color": theme("colors.primary.400 / 1"),
+            },
           },
         },
       }),
+      animation: {
+        "spin-slow": "spin 20s linear infinite",
+      },
+      keyframes: {
+        spin: {
+          from: {
+            transform: "rotate(0deg)",
+          },
+          to: {
+            transform: "rotate(360deg)",
+          },
+        },
+      },
     },
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require('@tailwindcss/forms')
-  ],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
